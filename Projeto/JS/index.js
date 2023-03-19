d4387ff7ca529b8bb88f978bcfc07338891cfe49
DMGS94 <95932699+DMGS94@users.noreply.github.com>
2023-03-19 22:17:30 +0000

    Add files via upload

---
+++ Projeto/JS/index.js
@@ -0,0 +1,8 @@
+function showform() {
+    var form = document.getElementById("formularioRegisto");
+    form.style.display = "block";
+  }
+function closeform() {
+    var form = document.getElementById("formularioRegisto");
+    form.style.display = "none";
+}
